dfd45cbb8519db20617ff98cfa703612c1f9587f
drethesilva <andressilva03@gmail.com>
2024-09-05 00:41:23 +0100

    Google Ad Sense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,49 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import "../styles/main.css";
-import type {Metadata} from "next";
-import {Roboto, Source_Code_Pro, Inter, Ubuntu} from "next/font/google";
+import type { Metadata } from "next";
+import { Roboto, Source_Code_Pro, Inter, Ubuntu } from "next/font/google";
 import Layout from "@/components/globals/Layout";
+import GoogleAdsense from "@/components/google/GoogleAdSense";
 
 const roboto = Roboto({
-   subsets: ["latin"],
-   weight: ["300", "400", "500", "700"],
-   variable: "--font-roboto",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-roboto",
 });
 
 const sourceCodePro = Source_Code_Pro({
-   subsets: ["latin"],
-   weight: ["400", "600", "700"],
-   variable: "--font-source-code-pro",
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  variable: "--font-source-code-pro",
 });
 
 const inter = Inter({
-   subsets: ["latin"],
-   weight: ["300", "400", "500", "700"],
-   variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-inter",
 });
 
 const ubuntu = Ubuntu({
-   subsets: ["latin"],
-   weight: ["300", "400", "500", "700"],
-   variable: "--font-ubuntu",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-ubuntu",
 });
 
 const fonts = [roboto, sourceCodePro, inter, ubuntu];
 
 export const metadata: Metadata = {
-   title: "Falcon",
-   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-   icons: "/images/favicon.png",
+  title: "Falcon",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  icons: "/images/favicon.png",
 };
 
-export default function RootLayout({children}: {children: ReactNode}) {
-   return (
-      <html lang="en">
-         <body className={fonts.map((font) => font.variable).join(" ")}>
-            <Layout>{children}</Layout>
-         </body>
-      </html>
-   );
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={fonts.map((font) => font.variable).join(" ")}>
+        <Layout>{children}</Layout>
+      </body>
+      <GoogleAdsense pId="4038932100311065" />
+    </html>
+  );
 }
